ee0d56a0662d369df9c748f25b082bc7e1c66efa
مطور _نظام_العرب <247216366+nadr47892-droid@users.noreply.github.com>
2026-03-28 16:21:15 +0200

    Delete اللغة البرمجية العربية/التنفيذ/المحرك_الرياضي directory

--- اللغة البرمجية العربية/التنفيذ/المحرك_الرياضي/العمليات_الحسابية_الاساسية/المعادلات_الاسية/محرك_الاسس.h
+++
@@ -1,6 +0,0 @@
-#ifndef POWER_ENGINE_H
-#define POWER_ENGINE_H
-
-int power(double *v);
-
-#endif
--- اللغة البرمجية العربية/التنفيذ/المحرك_الرياضي/العمليات_الحسابية_الاساسية/محرك_العمليات_الحسابية_الاساسية.h
+++
@@ -1,8 +0,0 @@
-#ifndef BASIC_ARITH_ENGINE_H
-#define BASIC_ARITH_ENGINE_H
-
-int basic_expression(double *v);
-int term(double *v);
-
-
-#endif
